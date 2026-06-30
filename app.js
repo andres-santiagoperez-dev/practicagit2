b96a69bf0221bfb004b7d4a701ece9b51fc2f280
andres-santiagoperez-dev <andresperez9276@gmail.com>
2026-06-30 16:31:30 -0500

    Agregmos 3 ciudades mas al arreglo de objetos ciudades

--- app.js
+++ app.js
@@ -31,6 +31,39 @@ let ciudades =[
             "01":"motel",
             "02":"zona rosa"
         }
+    },
+    {
+        nombre:"Obando",
+        numeroHabitantes:10000,
+        metrosSobreNivelMar:50,
+        temperaturaPromedio:30,
+        coordenadas:[6.2442, -75.5812],
+        sitiosDeInteres:{
+            "01":"parque",
+            "02":"zoologico"
+        }
+    },
+    {
+        nombre:"Jamundi",
+        numeroHabitantes:90000,
+        metrosSobreNivelMar:80,
+        temperaturaPromedio:32,
+        coordenadas:[4.2442, -50.5812],
+        sitiosDeInteres:{
+            "01":"Piscina",
+            "02":"Tienda de Ropa"
+        }
+    },
+    {
+        nombre:"Cali",
+        numeroHabitantes:150000,
+        metrosSobreNivelMar:120,
+        temperaturaPromedio:34,
+        coordenadas:[1.2442, -95.5812],
+        sitiosDeInteres:{
+            "01":"motel",
+            "02":"zona rosa"
+        }
     }
 ];
 
