2d4753e014bdc646082daff01b41355928096764
andres-santiagoperez-dev <andresperez9276@gmail.com>
2026-06-30 15:46:07 -0500

    Creamos el objeto de ciudades en el js

---
+++ app.js
@@ -0,0 +1,13 @@
+let ciudades =[
+    {
+        nombre:"Cartago",
+        numeroHabitantes:10000,
+        metrosSobreNivelMar:50,
+        temperaturaPromedio:30,
+        coordenadas:[],
+        sitiosDeInteres:{
+            "01":"parque",
+            "02":"zoologico"
+        }
+    }
+];
